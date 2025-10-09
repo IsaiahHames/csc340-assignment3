b3010240555b66efab3fc1b913c70859bfed55c9
IsaiahHames <isaiah.hames14@gmail.com>
2025-10-09 19:28:43 -0400

    initial commit

---
+++ src/test/java/com/bigcats/assignemtn/BigcatsGalleryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bigcats.assignemtn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BigcatsGalleryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
